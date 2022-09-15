2736141f062f6f8111be00c361d74d63947db874
Jonathan Hume <jonathan.hume@gmail.com>
2022-09-15 11:11:39 +0100

    Initial Commit

---
+++ swiftuiUndoableTextEditor/swiftuiUndoableTextEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiUndoableTextEditorApp.swift
+//  swiftuiUndoableTextEditor
+//
+//  Created by Jonathan Hume on 15/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiUndoableTextEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
